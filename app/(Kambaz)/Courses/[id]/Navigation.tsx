f99f9e553ef978987f44d38b1b1c74c2a5e19200
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-09-16 08:22:59 -0400

    fixed course nav

--- app/(Kambaz)/Courses/[id]/Navigation.tsx
+++ app/(Kambaz)/Courses/[id]/Navigation.tsx
@@ -5,13 +5,13 @@ export default function CourseNavigation({ id }: { id: string }) {
       <Link href={`/Courses/${id}/Home`} id="wd-course-home-link">Home</Link><br/>
       <Link href={`/Courses/${id}/Modules`} id="wd-course-modules-link">Modules
         </Link><br/>
-      <Link href={`/Courses/${id}/Piazza`} id="wd-course-piazza-link">Piazza</Link><br/>
-      <Link href={`/Courses/${id}/Zoom`} id="wd-course-zoom-link">Zoom</Link><br/>
+      <Link href={`/Courses/${id}/Home`} id="wd-course-piazza-link">Piazza</Link><br/>
+      <Link href={`/Courses/${id}/Home`} id="wd-course-zoom-link">Zoom</Link><br/>
       <Link href={`/Courses/${id}/Assignments`} id="wd-course-quizzes-link">
           Assignments</Link><br/>
-      <Link href={`/Courses/${id}/Quizzes`} id="wd-course-assignments-link">Quizzes
+      <Link href={`/Courses/${id}/Home`} id="wd-course-assignments-link">Quizzes
         </Link><br/>
-      <Link href={`/Courses/${id}/Grades`} id="wd-course-grades-link">Grades</Link><br/>
-      <Link href={`/Courses/${id}/People/Table`} id="wd-course-people-link">People</Link><br/>
+      <Link href={`/Courses/${id}/Home`} id="wd-course-grades-link">Grades</Link><br/>
+      <Link href={`/Courses/${id}/Home`} id="wd-course-people-link">People</Link><br/>
     </div>
   );}
