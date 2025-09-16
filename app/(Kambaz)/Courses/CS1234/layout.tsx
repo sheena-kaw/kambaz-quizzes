852abde55c860168d09e62b3c2f5d10a757695d0
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-09-15 21:11:34 -0400

    layout fix

--- app/(Kambaz)/Courses/CS1234/layout.tsx
+++ app/(Kambaz)/Courses/CS1234/layout.tsx
@@ -1,23 +1,3 @@
-// import { ReactNode } from "react";
-// import CourseNavigation from "./Navigation";
-// export default function CoursesLayout(
-//   { children, params }: { children: ReactNode; params: { CS1234: string } }) {
-//  const { CS1234 } = params;
-//  return (
-//    <div id="wd-courses">
-//      <h2>Course CS1234 {CS1234}</h2> 
-//      <hr />
-//      <table>
-//        <tbody>
-//          <tr>
-//            <td valign="top" width="200"> <CourseNavigation /> </td>
-//            <td valign="top" width="100%"> {children} </td>
-//          </tr>
-//        </tbody>
-//      </table>
-//    </div>
-// );}
-
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 export default async function CoursesLayout(
