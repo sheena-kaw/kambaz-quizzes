b300fe37f7234dcf57eb21e22aa759ba74ad379f
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-09-29 17:25:45 -0400

    lab done

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,107 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et lorem
+        suscipit, malesuada nibh ac, pellentesque magna. Nam augue lectus,
+        bibendum sed lacus quis, rutrum imperdiet ante. Fusce commodo nec augue
+        semper suscipit. Quisque ullamcorper felis aliquet tortor pellentesque
+        laoreet. Integer eu lacus pharetra, consectetur nisl eget, semper risus.
+        Pellentesque et tristique ligula. Mauris elementum diam sem, quis
+        laoreet orci mollis et. Mauris eget eros luctus, scelerisque ante eu,
+        condimentum nulla. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec et lorem suscipit, malesuada nibh ac, pellentesque magna.
+        Nam augue lectus, bibendum sed lacus quis, rutrum imperdiet ante. Fusce
+        commodo nec augue semper suscipit. Quisque ullamcorper felis aliquet
+        tortor pellentesque laoreet. Integer eu lacus pharetra, consectetur nisl
+        eget, semper risus. Pellentesque et tristique ligula. Mauris elementum
+        diam sem, quis laoreet orci mollis et. Mauris eget eros luctus,
+        scelerisque ante eu, condimentum nulla.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et lorem
+        suscipit, malesuada nibh ac, pellentesque magna. Nam augue lectus,
+        bibendum sed lacus quis, rutrum imperdiet ante. Fusce commodo nec augue
+        semper suscipit. Quisque ullamcorper felis aliquet tortor pellentesque
+        laoreet. Integer eu lacus pharetra, consectetur nisl eget, semper risus.
+        Pellentesque et tristique ligula. Mauris elementum diam sem, quis
+        laoreet orci mollis et. Mauris eget eros luctus, scelerisque ante eu,
+        condimentum nulla. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec et lorem suscipit, malesuada nibh ac, pellentesque magna.
+        Nam augue lectus, bibendum sed lacus quis, rutrum imperdiet ante. Fusce
+        commodo nec augue semper suscipit. Quisque ullamcorper felis aliquet
+        tortor pellentesque laoreet. Integer eu lacus pharetra, consectetur nisl
+        eget, semper risus. Pellentesque et tristique ligula. Mauris elementum
+        diam sem, quis laoreet orci mollis et. Mauris eget eros luctus,
+        scelerisque ante eu, condimentum nulla.
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et lorem
+        suscipit, malesuada nibh ac, pellentesque magna. Nam augue lectus,
+        bibendum sed lacus quis, rutrum imperdiet ante. Fusce commodo nec augue
+        semper suscipit. Quisque ullamcorper felis aliquet tortor pellentesque
+        laoreet. Integer eu lacus pharetra, consectetur nisl eget, semper risus.
+        Pellentesque et tristique ligula. Mauris elementum diam sem, quis
+        laoreet orci mollis et. Mauris eget eros luctus, scelerisque ante eu,
+        condimentum nulla. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec et lorem suscipit, malesuada nibh ac, pellentesque magna.
+        Nam augue lectus, bibendum sed lacus quis, rutrum imperdiet ante. Fusce
+        commodo nec augue semper suscipit. Quisque ullamcorper felis aliquet
+        tortor pellentesque laoreet. Integer eu lacus pharetra, consectetur nisl
+        eget, semper risus. Pellentesque et tristique ligula. Mauris elementum
+        diam sem, quis laoreet orci mollis et. Mauris eget eros luctus,
+        scelerisque ante eu, condimentum nulla.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et lorem
+        suscipit, malesuada nibh ac, pellentesque magna. Nam augue lectus,
+        bibendum sed lacus quis, rutrum imperdiet ante. Fusce commodo nec augue
+        semper suscipit. Quisque ullamcorper felis aliquet tortor pellentesque
+        laoreet. Integer eu lacus pharetra, consectetur nisl eget, semper risus.
+        Pellentesque et tristique ligula. Mauris elementum diam sem, quis
+        laoreet orci mollis et. Mauris eget eros luctus, scelerisque ante eu,
+        condimentum nulla. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec et lorem suscipit, malesuada nibh ac, pellentesque magna.
+        Nam augue lectus, bibendum sed lacus quis, rutrum imperdiet ante. Fusce
+        commodo nec augue semper suscipit. Quisque ullamcorper felis aliquet
+        tortor pellentesque laoreet. Integer eu lacus pharetra, consectetur nisl
+        eget, semper risus. Pellentesque et tristique ligula. Mauris elementum
+        diam sem, quis laoreet orci mollis et. Mauris eget eros luctus,
+        scelerisque ante eu, condimentum nulla.
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
