9b9b5d2fa473d6c08f8c518070066250a7a48236
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-09-29 17:55:00 -0400

    fix lab errors

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -29,7 +29,7 @@ export default function Lab2() {
       <br />
       <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although it&apos;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
       <div id="wd-css-id-selectors">
@@ -40,7 +40,7 @@ export default function Lab2() {
           name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
+          Here&apos;s another paragraph using a different ID and a different look and
           feel
         </p>
       </div>
@@ -48,7 +48,7 @@ export default function Lab2() {
         <br />
         <h3>Class selectors</h3>
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's
+          Instead of using IDs to refer to elements, you can use an element&apos;s
           CLASS attribute
         </p>
         <h4 className="wd-class-selector">
@@ -63,7 +63,7 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular places in
             the document
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
+              This paragraph&apos;s red background is referenced as
               <br />
               .selector-2 .selector3
               <br />
