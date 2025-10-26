99b892ea604c9dcbb73f0de008b23423bc198680
ScarlettBlaize <cutiepie.ats@gmail.com>
2025-10-26 15:53:29 -0400

    client fix??

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import { ReactNode } from "react";
 import TOC from "./TOC";
-
+import { Provider } from "react-redux";
+import store from "./Lab4/store";
 
 export default function LabsLayout({
- children,
+  children,
 }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
+  return (
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
+  );
+}
